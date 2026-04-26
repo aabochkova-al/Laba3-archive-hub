eab76d0b140ed96497013b2aa83fec98111f7dfc
Alexandra <aabochkova@gmail.com>
2026-04-26 16:08:01 +0300

    Создание примерной структуры для лабы 3

---
+++ src/main/java/mission/laba3/entity/CurseEntity.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mission.laba3.entity;
+
+/**
+ *
+ * @author aleksandra
+ */
+public class CurseEntity {
+    
+}
